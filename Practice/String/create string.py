6ee3fc3d693ee3a0bb413df358b9ba87291a3158
sina karimi <sinak5081@gmail.com>
2025-12-16 15:18:37 -0800

    Methods for creating strings

---
+++ Practice/String/create string.py
@@ -0,0 +1,14 @@
+#first method
+S1 = "Computer science"
+S2 = "Software Engineering"
+#second method
+S3 = str("Python programming")
+print(S1)
+print(S2)
+print(S3)
+print("_________________________")
+#third method
+s4 = """Python is a modern language
+writing program in this language is a good practice.
+We are learning python language"""
+print(s4)
