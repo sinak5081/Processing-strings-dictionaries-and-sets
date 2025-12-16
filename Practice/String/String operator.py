0c56db634b3ffc20e5f4ec34bc3c185046282374
sina karimi <sinak5081@gmail.com>
2025-12-16 15:30:25 -0800

    String operators

---
+++ Practice/String/String operator.py
@@ -0,0 +1,23 @@
+#operator +
+s1 = "www."
+s2 = "python."
+s3 = "org"
+print(s1+s2+s3)
+# operator *
+s = "python" * 5
+print(s)
+# operator []
+s4 = "Computer"
+print(s4[2],s4[-3])
+#String cutting with operator [n:m]
+print(s4[:2])
+print(s4[2:])
+print(s4[2:4])
+#operator in
+s5 = "Python programming"
+print("Python" in s5)
+print("computer" in s5)
+print("_____________________")
+#operator not in
+print("Python" not in s5)
+print("computer" not in s5)
