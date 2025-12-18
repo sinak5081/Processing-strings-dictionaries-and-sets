e8cb007f16ddda2849fa55a9c92df0358b379c71
sina karimi <sinak5081@gmail.com>
2025-12-18 12:05:39 -0800

    String formatting

---
+++ Practice/String/String formatting.py
@@ -0,0 +1,10 @@
+#String formatting
+print("{0:<6d}".format(356))
+print("{0:>6d}".format(356))
+print("__________________________")
+x = 34567861
+print("The value is {:,}".format(x))
+print("____________________________")
+print("{0:^6d}".format(35))
+print("______________________________")
+print("{0:+6d}.{1:-6d}".format(356,-452))
