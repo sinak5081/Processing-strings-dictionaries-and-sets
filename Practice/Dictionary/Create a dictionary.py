ef8efce92df76b43d2d8e4a919659f8f67f624af
sina karimi <sinak5081@gmail.com>
2025-12-22 17:39:52 -0800

    Methods for creating a dictionary

---
+++ Practice/Dictionary/Create a dictionary.py
@@ -0,0 +1,15 @@
+dic = {
+    "Iran" : "Tehran",
+    "America" : "washington",
+    "Germany" : "Berlin",
+    "France" : "Paris"
+}
+print(dic)
+#second method
+aveDic = dict([
+    (100,15.5),
+    (200,18.5),
+    (300,14.5),
+    (400,17.5)
+])
+print(aveDic)
